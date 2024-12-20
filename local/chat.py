97d5db7eefe4492cec13ed3bfdb9f40fc126dd0a
alpzc <143123316+alpzc@users.noreply.github.com>
2024-12-20 12:32:28 -0600

    added chat loop

--- local/chat.py
+++ local/chat.py
@@ -1,8 +1,11 @@
 from graph import app
 from pprint import pprint
 
-inputs = {"question": "What mechanism is used to implement attention in a neural network? Explain in detail."}
-for output in app.stream(inputs):
-    for key, value in output.items():
-        pprint(f"Finished running: {key}:")
-pprint(value["generation"])
+user_question = None
+while user_question != "exit":
+    user_question = input("Enter a question: ")
+    inputs = {"question": user_question}
+    for output in app.stream(inputs):
+        for key, value in output.items():
+            pprint(f"Finished running: {key}:")
+    pprint(value["generation"])
